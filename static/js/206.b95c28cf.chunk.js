3f9ba2a37f05e826bc8944b758d9435b7ff421b0
charivna <124678194+charivna@users.noreply.github.com>
2023-10-10 20:06:07 +0000

    Deploying to gh-pages from @ charivna/goit-react-hw-08-phonebook@870d306fe4261f735cea224d6ea73e455a467794 🚀

--- static/js/206.b95c28cf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{206:function(e,n,r){r.r(n),r.d(n,{default:function(){return a}});var t=r(184);function a(){return(0,t.jsx)("div",{children:(0,t.jsxs)("h1",{children:["ContactsBook welcome page"," ",(0,t.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
-//# sourceMappingURL=206.b95c28cf.chunk.js.map
